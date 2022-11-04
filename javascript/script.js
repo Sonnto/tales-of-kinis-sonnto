35632350f1db224ff785d167519d586ba26895bf
Sonnto <90372802+Sonnto@users.noreply.github.com>
2022-11-04 17:26:41 -0400

    add to storyText array, add to functions

--- javascript/script.js
+++ javascript/script.js
@@ -9,7 +9,7 @@ let sonnto = {};
 //Game-Starting Function
 function startGame() {
   sonnto = {}; //Character, Kinis Sonnto, object
-  showStoryText(1);
+  showStoryText(1); //Start game on first storyText
 }
 
 //Gets the story text to be displayed
@@ -37,7 +37,7 @@ function showStoryText(storyTextIndex) {
 }
 
 function showChoice(choice) {
-  return choice.requiredState == null || choice.requiredState(state);
+  return choice.requiredSonnto == null || choice.requiredSonnto(sonnto);
 }
 
 //Choice-Selecting Function, to know which choice player chooses
@@ -49,6 +49,7 @@ function selectChoice(choice) {
 
 //Array of objects which represent the different stories/pathways and choices
 const storyTexts = [
+  // STORY DEATHS
   {
     pathID: 100,
     text: "The ship's burning interior overheats the engine and the ship exlodes with you inside. You have died.",
@@ -57,9 +58,10 @@ const storyTexts = [
     pathID: 100,
     text: "Death scene 2",
   },
+  // 1 STORY CARD
   {
     pathID: 1,
-    text: 'You wake up in a strange location. You feel the cold, lifeless metal floor. There is a burning smell in the air and the lights are flickering - you deduce that you are within a downed ship. Your hands instantly move towards your utility belt in search of your weapon - it\'s gone. "This lightsabre is your life" - the words of your old master echoes in your head as you scour the area in search of it. You finally come across the hilt sitting beside a collapsed pillar.',
+    text: 'You wake up in a strange location. You feel the cold, lifeless metal floor. There is a burning smell in the air and the lights are flickering - you deduce that you are within a downed ship. Your hands instantly move towards your utility belt in search of your weapon - it\'s gone.\n\n"This lightsabre is your life" - the words of your old master echoes in your mind as you scour the area in search of it. You finally come across the hilt sitting beside a collapsed pillar.',
     choices: [
       {
         text: "[take lightsabre hilt]",
@@ -71,14 +73,14 @@ const storyTexts = [
         nextText: 2,
       },
     ],
-  },
+  }, // 2 STORY CARD
   {
     pathID: 2,
-    text: "You scan your surrounding. You spot a datapad with the Jedi Order's insignia on the back. You pick it up. It reads the following:\n| Name: <Kinis Sonnto> \n| Rank: <Jedi Knight> \n| Mission: <Seek out and neutralize reported dark sider on Nar Shaddaa.> \n| Support: <Strike Team: 3 Republic Soldiers.>}\nYou place the datapad on your belt as you notice the blaster burns on the walls and the fire starting to spread towards the engine bay.",
+    text: "You scan your surrounding. You spot a datapad with the Jedi Order's insignia on the back. You pick it up. It reads the following:\n\n| >Name: Kinis Sonnto \n| >Rank: Jedi Knight \n| >Mission: Seek out and neutralize reported dark sider on Nar Shaddaa. \n| >Support: Strike Team: 3 Republic Soldiers.}\n\nYou place the datapad on your belt as you notice the blaster burns on the walls and the fire starting to spread towards the engine bay. Your Jedi senses allow you to notice three blaster rifles on the ground by the cockpit of the ship.",
     choices: [
       {
-        text: "[take the three blasters and leave the ship immediately]",
-        setSonnto: { blaster1: true, blaster2: true, blaster3: true },
+        text: "[force pull the three blasters to you and leave the ship immediately]",
+        setSonnto: { blasters: true },
         nextText: 3,
       },
       {
@@ -89,13 +91,27 @@ const storyTexts = [
         text: "[stay inside the downed ship]",
         nextText: 100,
       },
+      {
+        text: "[move towards the cockpit and investigate]",
+        nextText: 100,
+      },
     ],
   },
   {
     pathID: 3,
-    text: "Third Text",
+    text: "You conjure the Force and pull the blasters to you as you exit the ship. Your haste in leaving turns out for the better, as the ship exlodes behind you. You notice the three Republic soldiers coming to and getting up. You approach them.",
+    choices: [
+      {
+        text: "[Give the three blasters to each soldier.",
+        requiredSonnto: (currentSonnto) => currentSonnto.blasters,
+        setSonnto: { blasters: false, soldiersArmed: true },
+        nextText: 4,
+      },
+    ],
   },
 ];
 
 //Starts Game
 startGame();
+
+console.log(sonnto);
